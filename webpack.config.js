f26c08880fc6074ac518e3bdc6e4dee1bdef6445
Edvard <mejik-yo@mail.ru>
2019-11-08 16:13:12 +0300

    init: create project structure

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'test-task',
+    meta: { viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
+  })],
+};
